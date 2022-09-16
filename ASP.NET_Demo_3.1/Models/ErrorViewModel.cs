57eb7d4542e94e0f68399a5964f201afdf6515e8
Amoriss <amoriss@protonmail.com>
2022-09-15 22:48:31 -0500

    Add project files.

---
+++ ASP.NET_Demo_3.1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP.NET_Demo_3._1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
